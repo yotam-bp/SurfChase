0581376368a38c2712f3368b0d24f06b7c1f8620
yotam-bp <yotyyybp@gmail.com>
2024-06-04 23:20:53 +0300

    fix image size in location details

--- app/(root)/locations/[id]/page.tsx
+++ app/(root)/locations/[id]/page.tsx
@@ -12,18 +12,17 @@ const LocationDetails = async ({
   searchParams,
 }: SearchParamProps) => {
   const location: ILocation = await getLocationIdById(id);
+
   const firstMonth = location.seasons[1].months[0];
   const lastMonthIndex = location.seasons[1].months.length - 1;
   const lastMonth = location.seasons[1].months[lastMonthIndex];
   const currentMonth: string = getCurrentMonth();
 
   const getTemperatures = () => {
-    const monthData = location.monthlyTemperatures.entries?.find(
-      (item) => item.month === currentMonth
-    );
+    const monthData = location.monthlyTemperatures.entries?.find(item => item.month === currentMonth); 
     return {
       seaTemp: monthData?.seaTemperature,
-      outsideTemp: monthData?.outsideTemperature,
+      outsideTemp: monthData?.outsideTemperature
     };
   };
 
@@ -31,14 +30,14 @@ const LocationDetails = async ({
     <>
       <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
         <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
-          <div className="relative h-[300px] w-full md:h-auto">
+          <div className="relative w-full aspect-w-16 aspect-h-9 md:aspect-h-auto md:h-auto">
             <Image
               src={location.imageUrl}
               alt="hero image"
               layout="fill"
               objectFit="cover"
               objectPosition="center"
-              className="rounded-t-xl md:rounded-none md:rounded-l-xl"
+              className="h-full w-full"
               loading="lazy"
             />
           </div>
@@ -48,7 +47,7 @@ const LocationDetails = async ({
               <h2 className="h2-bold">{location.name}</h2>
 
               <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
-                <div className="flex flex-wrap gap-3">
+                <div className="flex gap-3">
                   <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
                     {location.country}
                   </p>
@@ -85,21 +84,14 @@ const LocationDetails = async ({
             </div>
 
             <div className="flex flex-col gap-2">
-              <p className="p-bold-20 text-grey-600">
-                weather on {currentMonth}:
-              </p>
-              <p className="p-medium-16 lg:p-regular-18">
-                🌡️ Outside Temperatures {getTemperatures().outsideTemp}
-              </p>
-              <p className="p-medium-16 lg:p-regular-18">
-                🌊 Sea Temperatures {getTemperatures().seaTemp}
-              </p>
+              <p className="p-bold-20 text-grey-600">weather on {currentMonth}:</p>
+              <p className="p-medium-16 lg:p-regular-18">🌡️ Outside Tempratures {getTemperatures().outsideTemp}</p>
+              <p className="p-medium-16 lg:p-regular-18 ">🌊 Sea Tempratures {getTemperatures().seaTemp}</p>
             </div>
           </div>
         </div>
       </section>
 
-      {/* EVENTS with the same category */}
       <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         {/* <h2 className="h2-bold">Related Events</h2> */}
 
