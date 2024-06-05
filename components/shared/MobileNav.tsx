2ffded7bab11bad787324351c05b1733d0750130
yotam-bp <yotyyybp@gmail.com>
2024-06-05 18:08:38 +0300

    fixing Mobile nav

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,26 +1,32 @@
+"use client"
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Image from "next/image";
 import { Separator } from "../ui/separator";
+import { useState } from 'react';
 import NavItems from "./NavItems";
 
-const MobileNav = () => {
+const MobileNav: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="md:hidden">
-      <Sheet>
-        <SheetTrigger className="align-middle">
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger asChild>
           <Image
             src="/assets/icons/menu.svg"
             alt="menu"
             width={24}
             height={24}
             className="cursor-pointer"
+            onClick={() => setIsOpen(true)}
           />
         </SheetTrigger>
         <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
@@ -31,7 +37,7 @@ const MobileNav = () => {
             height={38}
           />
           <Separator className="border border-gray-50" />
-            <NavItems />
+          <NavItems onClose={handleClose} />
         </SheetContent>
       </Sheet>
     </nav>
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,31 +1,35 @@
-'use client';
+"use client"
+import { headerLinks } from '@/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
 
-import { headerLinks } from '@/constants'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
+interface NavItemsProps {
+  onClose: () => void;
+}
 
-const NavItems = () => {
+const NavItems: React.FC<NavItemsProps> = ({ onClose }) => {
   const pathname = usePathname();
 
   return (
     <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
       {headerLinks.map((link) => {
         const isActive = pathname === link.route;
-        
+
         return (
           <li
             key={link.route}
             className={`${
-              isActive && 'text-primary-500'
-            } flex-center p-medium-16 whitespace-nowrap`}
+              isActive ? 'text-primary-500' : ''
+            } flex-center p-medium-16 whitespace-nowrap cursor-pointer`}
+            onClick={onClose}
           >
             <Link href={link.route}>{link.label}</Link>
           </li>
-        )
+        );
       })}
     </ul>
-  )
-}
+  );
+};
 
-export default NavItems
+export default NavItems;
